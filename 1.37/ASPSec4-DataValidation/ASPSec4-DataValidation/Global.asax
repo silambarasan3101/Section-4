80bb73af237cfb34e8b07570f2f8f728c3b3c984
Silambarasan <msilambarasan98@gmail.com>
2023-09-06 15:07:38 +0530

    first commit

---
+++ 1.37/ASPSec4-DataValidation/ASPSec4-DataValidation/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASPSec4_DataValidation.MvcApplication" Language="C#" %>
---
+++ 1.39/CustomHelperMethods/CustomHelperMethods/Views/Home/CustomHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CustomHelperMethods.Views.Home
+{
+    public class CustomHelper
+    {
+        public static HtmlString File(string id)
+        {
+            TagBuilder tb = new TagBuilder("input");
+            tb.Attributes.Add("type", "file");
+            tb.Attributes.Add("id", id);
+            return new HtmlString(tb.ToString());
+        }
+
+    }
+}
